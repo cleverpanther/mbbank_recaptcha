4ab769ef729b9e0ec749b5b1b3dfc1d5160fa151
cleverpanther <cleverpanther0819@gmail.com>
2024-06-24 12:42:59 -0700

    'v.1.1'

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import numpy as np
 import tensorflow as tf
 import os
 import keras
+import base64
+import io
+from PIL import Image
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
 from keras import layers, ops
@@ -12,7 +15,7 @@ app = Flask(__name__)
 prediction_model = tf.keras.models.load_model('my_model.keras')
 img_height = 200
 img_width = 35
-characters = ['2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'R', 'T', 'Y', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'q', 'r', 't', 'y']
+characters = ['1','2','3','4','5','6','7','8','9','A','B','D','E','F','G','H','J','M','N','R','T','Y','a','b','d','e','f','g','h','j','m','n','q','r','t','y']
 max_length = 6
 
 char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
@@ -69,7 +72,6 @@ def decode_batch_predictions(pred):
     results = ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
         :, :max_length
     ]
-    print(results)
     # Iterate over the results and get back the text
     output_text = []
     for res in results:
@@ -77,14 +79,28 @@ def decode_batch_predictions(pred):
         output_text.append(res)
     return output_text
 
+def decode_base64_image(base64_string):
+    if base64_string.startswith("data:image/png;base64,"):
+        base64_string = base64_string.replace("data:image/png;base64,", "")
+    
+    image_data = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(image_data))
+    image_path = 'temp.png'
+    image.save(image_path)
+    
+    return image_path
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'})
-
-    image_file = request.files['image']
-    image_path = 'temp.png'
-    image_file.save(image_path)  # Save the uploaded image
+    if 'image' in request.files:
+        image_file = request.files['image']
+        image_path = 'temp.png'
+        image_file.save(image_path)
+    elif 'image_base64' in request.json:
+        base64_string = request.json['image_base64']
+        image_path = decode_base64_image(base64_string)
+    else:
+        return jsonify({"error": "No image provided."})
 
     try:
         encoded_image = encode_image(image_path)
